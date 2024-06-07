cffbf112a4ea0713da625243b81a8d8ce0c4ba02
Kaustubh <kaustubhrx@gmail.com>
2024-06-07 18:55:07 +0530

    Add Active NavLinks

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,10 +1,15 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
 import logo from "../../../public/Pix3l_Wear.png";
 import Link from "next/link";
 import "../../app/globals.css";
+import { usePathname } from "next/navigation";
 
 export const Navbar = () => {
+  const pathName = usePathname();
+
   return (
     <nav
       className="Navbar bg-black flex justify-between font"
@@ -16,22 +21,52 @@ export const Navbar = () => {
       <div className="flex items-center pr-8">
         <ul className="flex text-white space-x-6 text-2xl cursor-move">
           <li>
-            <Link href="/">Home</Link>
+            <Link
+              href="/"
+              className={`link ${pathName === "/" ? "active" : ""}`}
+            >
+              Home
+            </Link>
           </li>
           <li>
-            <Link href="/clothing">Clothing</Link>
+            <Link
+              href="/clothing"
+              className={`link ${pathName === "/clothing" ? "active" : ""}`}
+            >
+              Clothing
+            </Link>
           </li>
           <li>
-            <Link href="/shipping">Shipping</Link>
+            <Link
+              href="/shipping"
+              className={`link ${pathName === "/shipping" ? "active" : ""}`}
+            >
+              Shipping
+            </Link>
           </li>
           <li>
-            <Link href="/contact">Contact</Link>
+            <Link
+              href="/contact"
+              className={`link ${pathName === "/contact" ? "active" : ""}`}
+            >
+              Contact
+            </Link>
           </li>
           <li>
-            <Link href="/cart">Cart</Link>
+            <Link
+              href="/cart"
+              className={`link ${pathName === "/cart" ? "active" : ""}`}
+            >
+              Cart
+            </Link>
           </li>
           <li>
-            <Link href="/register">SIGN IN</Link>
+            <Link
+              href="/register"
+              className={`link ${pathName === "/register" ? "active" : ""}`}
+            >
+              SIGN IN
+            </Link>
           </li>
         </ul>
       </div>
