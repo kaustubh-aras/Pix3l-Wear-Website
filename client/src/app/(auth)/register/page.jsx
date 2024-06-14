61fbd3642c90161939b28f0fad738ac8191d0272
Kaustubh <kaustubhrx@gmail.com>
2024-06-15 01:20:29 +0530

    Created Register Page

--- client/src/app/(auth)/register/page.jsx
+++ client/src/app/(auth)/register/page.jsx
@@ -1,14 +1,51 @@
+"use client";
+
+import Input from "@/components/input/Input";
+import Link from "next/link";
+import React, { useState } from "react";
 import Image from "next/image";
-import React from "react";
 
 function Register() {
-  return (
-    <div className="bg-black text-white flex flex-col items-center justify-center">
-      <div className="flex flex-col items-center">
-        <Image src="/Pix3l_Wear.png" alt="logo" width={100} height={100} />
-      </div>
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
 
-      <label className="pt-7">CREATE ACCOUNT</label>
+  return (
+    <div
+      className="bg-black text-white flex flex-col items-center justify-center min-h-screen"
+      style={{ fontFamily: "Klapt-Regular" }}
+    >
+      <div className="flex flex-col">
+        <div className="flex items-center justify-center pb-3">
+          <Image src="/Pix3l_Wear_2.png" width={150} height={100} />
+        </div>
+        <label className="text-xl ml-4 font-bold">Sign In</label>
+        <div className="flex flex-col">
+          <Input
+            type="text"
+            placeholder="login"
+            value={login}
+            onChange={(e) => setLogin(e.target.value)}
+          />
+          <Input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Link href="/contact" className="ml-3">
+            Forgot Password
+          </Link>
+          <button className="bg-white text-black m-3 p-3 rounded-2xl items-center justify-center">
+            <h1 className="text-xl font-bold">Click ME</h1>
+          </button>
+          <div className="flex item-center justify-center gap-3">
+            <span className="text-slate-300">Don't have an account?</span>
+            <span>
+              <Link href="/">Click here to create one</Link>
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
