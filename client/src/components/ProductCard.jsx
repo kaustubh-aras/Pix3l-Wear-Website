4a4ccdc9e7de6342cfcd20965d6c5fadb0dd65ab
Kaustubh <kaustubhrx@gmail.com>
2024-06-16 00:32:43 +0530

    Fix Bugs

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -4,10 +4,10 @@ export const ProductCard = ({ product }) => {
   return (
     <div
       style={{ fontFamily: "Klapt-Regular" }}
-      className="max-w-sm roundedflex items-center justify-center flex-col"
+      className="max-w-sm rounded flex items-center justify-center flex-col"
     >
       <div className="bg-gray-500 flex items-center justify-center rounded-[40px]">
-        <Image src="/product.png" alt="Products" />
+        <Image src="/product.png" alt="Products" width={500} height={500} />
       </div>
       <div className="text-xl mt-2 text-white flex items-center justify-center">
         {product.name}
