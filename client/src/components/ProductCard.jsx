f613cc3a27b74fdf3e4dc4fb4ded2b89ff91c067
Kaustubh <kaustubhrx@gmail.com>
2024-06-16 20:54:12 +0530

    Modified Product Card

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 
-export const ProductCard = ({ product }) => {
+export const ProductCard = ({ product, onClick }) => {
   return (
     <div
       style={{ fontFamily: "Klapt-Regular" }}
       className="max-w-sm rounded flex items-center justify-center flex-col"
+      onClick={onClick}
     >
       <div className="bg-gray-500 flex items-center justify-center rounded-[40px]">
         <Image src="/product.png" alt="Products" width={500} height={500} />
